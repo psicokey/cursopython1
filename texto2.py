56d678d5a6873d0370efc96ec594d717cb4c8883
Keyberth <devkeyberth@gmail.com>
2025-10-06 13:04:23 -0400

    Booleanos

---
+++ texto2.py
@@ -0,0 +1,18 @@
+# Indice 01234
+texto = "Este es un texto"
+
+print(texto[:-2])
+
+curso = "Este es un curso de Javascript"
+
+print(curso.replace("Javascript", "Python"))
+
+textoDividido = texto.split(" ")
+
+print(textoDividido)
+
+# Normalizacion
+
+texto2 = "Este texto tiene MAYUSCULAS y minusculas y necesito encontrar cierta palabra"
+
+print("mayusculas".lower() in texto2.lower())
