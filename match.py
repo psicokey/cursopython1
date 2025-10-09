24fb5162599d1a2e36485590f5b0dd9ea558fe3d
Keyberth <devkeyberth@gmail.com>
2025-10-09 10:48:36 -0400

    match

---
+++ match.py
@@ -0,0 +1,11 @@
+dia = 3
+
+match dia:
+    case 1:
+        print("Hoy es Lunes")
+    case 2:
+        print("Hoy es Martes")
+    case 3:
+        print("Hoy es Miércoles")
+    case _:
+        print("No coincide con las opciones anteriores")
