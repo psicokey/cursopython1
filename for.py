123ed60944cf4871ee5bd17ce5774beb436b6a5e
Keyberth <devkeyberth@gmail.com>
2025-10-10 12:23:27 -0400

    while y for

---
+++ for.py
@@ -0,0 +1,32 @@
+palabra = "python"
+
+for letra in palabra:
+    (print(letra))
+
+adjetivos = ["rica", "saludable"]
+frutas = ["manzana", "naranja", "kiwi"]
+
+for fruta in frutas:
+    for adjetivo in adjetivos:
+        print(fruta, adjetivo)
+# for fruta in frutas:
+#     if fruta == "naranja":
+#         continue
+#     print(fruta)
+# else:
+#     print("Ya hemos terminado el bucle for")
+
+print("--------------------------")
+
+# Range
+
+# Comienza desde el 0 y termina en el numero que asignemos sin incluirlo
+
+# for i in range(10):
+#    print(i)
+
+# for i in range(3, 5):
+#    print(i)
+
+# for i in range(0, 10, 2):
+#    print(i)
---
+++ while.py
@@ -0,0 +1,9 @@
+i = 0
+
+while i < 10:
+    i += 1
+    if i == 5:
+        continue
+    print(i)
+else:
+    print("i dejó de ser menor a 10")
