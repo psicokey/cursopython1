2c551cd8cc038afb71cf82c5b31b51def5cc4f0e
Keyberth <devkeyberth@gmail.com>
2025-10-13 11:38:11 -0400

    modulos

---
+++ modulos/main.py
@@ -0,0 +1,6 @@
+from operaciones import sumar, restar, multiplicar, dividir
+
+print(sumar(5, 3))
+print(restar(5, 3))
+print(multiplicar(5, 3))
+print(dividir(10, 5))
---
+++ modulos/operaciones.py
@@ -0,0 +1,16 @@
+def sumar(a, b):
+    return a + b
+
+
+def restar(a, b):
+    return a - b
+
+
+def multiplicar(a, b):
+    return a * b
+
+
+def dividir(a, b):
+    if b == 0:
+        return "No se puede dividir entre cero"
+    return a / b
